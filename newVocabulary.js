857c322725d10b9cb42488c41f54144da780ccbc
unknown <anhtuan.phung.16@gmail.com>
2025-03-12 20:46:24 +0700

    -

--- newVocabulary.js
+++ newVocabulary.js
@@ -1,10 +1,108 @@
 export const vocabulary = [
     {
-      vn: "hải sản",
-      eng: "seafood",
-      type: "noun",
-      pronounce: "/ˈsiːfuːd/"
+      "vn": "hải sản",
+      "eng": "seafood",
+      "type": "noun",
+      "pronounce": "/ˈsiːfuːd/"
     },
+    {
+      "vn": "hẹp",
+      "eng": "narrow",
+      "type": "adjective",
+      "pronounce": "/ˈnærəʊ/"
+    },
+    {
+      "vn": "đơn giản",
+      "eng": "simply",
+      "type": "adverb",
+      "pronounce": "/ˈsɪmpli/"
+    },
+    {
+      "vn": "nướng(bánh)",
+      "eng": "bake",
+      "type": "verb",
+      "pronounce": "/beɪk/"
+    },
+    {
+      "vn": "chạy trốn",
+      "eng": "flee",
+      "type": "verb",
+      "pronounce": "/fliː/"
+    },
+    {
+      "vn": "cúi chào",
+      "eng": "bow",
+      "type": "verb",
+      "pronounce": "/baʊ/"
+    },
+    {
+      "vn": "cho phép",
+      "eng": "allow",
+      "type": "verb",
+      "pronounce": "/əˈlaʊ/"
+    },
+    {
+      "vn": "tránh",
+      "eng": "avoid",
+      "type": "verb",
+      "pronounce": "/əˈvɔɪd/"
+    },
+    {
+      "vn": "rất tiếc phải thông báo làm gì đó",
+      "eng": "regret + toV, Be sorry to V, Hate to V",
+      "type": "verb phrases",
+      "pronounce": "No specific pronunciation"
+    },
+    {
+      "vn": "cuộc đi bộ",
+      "eng": "the hike",
+      "type": "noun",
+      "pronounce": "/ðə haɪk/"
+    },
+    {
+      "vn": "lang thang",
+      "eng": "hike",
+      "type": "verb",
+      "pronounce": "/haɪk/"
+    },
+    {
+      "vn": "khi còn nhỏ",
+      "eng": "as a child",
+      "type": "phrase",
+      "pronounce": "No specific pronunciation"
+    },
+    {
+      "vn": "đề xuất, gợi ý làm một việc gì đó",
+      "eng": "suggest +Ving, recommend+Ving, propose +Ving, advise +Ving, encourage +Ving",
+      "type": "verb phrases",
+      "pronounce": "No specific pronunciation"
+    },
+    {
+      "vn": "không thể ngừng làm gì, không thể không làm gì",
+      "eng": "can't help +Ving, Can't resist + V-ing, Be unable to stop + V-ing, advise +Ving, Be compelled to + V",
+      "type": "verb phrases",
+      "pronounce": "No specific pronunciation"
+    },
+    {
+      "vn": "bị ép buộc, cảm thấy phải làm gì",
+      "eng": "Be compelled to + V",
+      "type": "verb phrase",
+      "pronounce": "/bɪ kəmˈpɛld tə/"
+    },
+    {
+      "vn": "khoảng thời gian",
+      "eng": "period of time",
+      "type": "noun phrase",
+      "pronounce": "/ˈpɪərɪəd əv taɪm/"
+    },
+    {
+      "vn": "đưa ra lời khuyên",
+      "eng": "give advice",
+      "type": "verb phrase",
+      "pronounce": "/ɡɪv ædˈvaɪs/"
+    }
+  ,
+ 
     {
       vn: "quyết định, đưa ra sự lựa chọn",
       eng: "made up my mind",
@@ -220,6 +318,189 @@ export const vocabulary = [
         eng: "globalization",
         type: "noun",
         pronounce: "/ˌɡləʊbəlaɪˈzeɪʃən/"
-      }
+      },
+      {
+        vn: "tấn công",
+        eng: "srike",
+        type: "noun",
+        pronounce: "/ˌɡləʊbəlaɪˈzeɪʃən/"
+      },
+        {
+          "vn": "nhìn vào",
+          "eng": "look at",
+          "type": "phrasal verb",
+          "pronounce": "/lʊk æt/"
+        },
+        {
+          "vn": "tìm kiếm",
+          "eng": "look for",
+          "type": "phrasal verb",
+          "pronounce": "/lʊk fɔːr/"
+        },
+        {
+          "vn": "chăm sóc",
+          "eng": "look after",
+          "type": "phrasal verb",
+          "pronounce": "/lʊk ˈæftər/"
+        },
+        {
+          "vn": "tra cứu",
+          "eng": "look up",
+          "type": "phrasal verb",
+          "pronounce": "/lʊk ʌp/"
+        },
+        {
+          "vn": "mong đợi",
+          "eng": "look forward to",
+          "type": "phrasal verb",
+          "pronounce": "/lʊk ˈfɔːrwərd tə/"
+        },
+        {
+          "vn": "cảnh giác, chú ý",
+          "eng": "look out",
+          "type": "phrasal verb",
+          "pronounce": "/lʊk aʊt/"
+        },
+        {
+          "vn": "điều tra",
+          "eng": "look into",
+          "type": "phrasal verb",
+          "pronounce": "/lʊk ˈɪntuː/"
+        },
+        {
+          "vn": "kiểm tra",
+          "eng": "look over",
+          "type": "phrasal verb",
+          "pronounce": "/lʊk oʊvər/"
+        },
+        {
+          "vn": "xem thường",
+          "eng": "look down on",
+          "type": "phrasal verb",
+          "pronounce": "/lʊk daʊn ɑːn/"
+        },
+        {
+          "vn": "ngưỡng mộ",
+          "eng": "look up to",
+          "type": "phrasal verb",
+          "pronounce": "/lʊk ʌp tə/"
+        },
+        {
+          "vn": "trông chờ, mong đợi",
+          "eng": "look to",
+          "type": "phrasal verb",
+          "pronounce": "/lʊk tə/"
+        },
+        {
+          "vn": "xem qua",
+          "eng": "look through",
+          "type": "phrasal verb",
+          "pronounce": "/lʊk θruː/"
+        },
+        {
+          "vn": "đặt, để",
+          "eng": "put",
+          "type": "verb",
+          "pronounce": "/pʊt/"
+        },
+        {
+          "vn": "đặt vào",
+          "eng": "put in",
+          "type": "phrasal verb",
+          "pronounce": "/pʊt ɪn/"
+        },
+        {
+          "vn": "đặt lên",
+          "eng": "put on",
+          "type": "phrasal verb",
+          "pronounce": "/pʊt ɒn/"
+        },
+        {
+          "vn": "đặt ra, đưa ra",
+          "eng": "put forward",
+          "type": "phrasal verb",
+          "pronounce": "/pʊt ˈfɔːwəd/"
+        },
+        {
+          "vn": "đặt xuống",
+          "eng": "put down",
+          "type": "phrasal verb",
+          "pronounce": "/pʊt daʊn/"
+        },
+        {
+          "vn": "đưa vào, đầu tư vào",
+          "eng": "put into",
+          "type": "phrasal verb",
+          "pronounce": "/pʊt ˈɪntuː/"
+        },
+        {
+          "vn": "đặt, giao cho ai đó",
+          "eng": "put someone through",
+          "type": "phrasal verb",
+          "pronounce": "/pʊt ˈsʌmwʌn θruː/"
+        },
+        {
+          "vn": "làm cho ai đó cảm thấy",
+          "eng": "put off",
+          "type": "phrasal verb",
+          "pronounce": "/pʊt ɒf/"
+        },
+        {
+          "vn": "đặt vào tầm quan sát, chú ý",
+          "eng": "put up",
+          "type": "phrasal verb",
+          "pronounce": "/pʊt ʌp/"
+        },
+        {
+          "vn": "cư trú tạm thời",
+          "eng": "put up with",
+          "type": "phrasal verb",
+          "pronounce": "/pʊt ʌp wɪð/"
+        },
+        {
+          "vn": "thực hiện một hành động",
+          "eng": "put into action",
+          "type": "phrasal verb",
+          "pronounce": "/pʊt ˈɪntuː ˈækʃən/"
+        },
+        {
+          "vn": "đặt ra tiêu chuẩn",
+          "eng": "put up a standard",
+          "type": "verb phrase",
+          "pronounce": "/pʊt ʌp ə ˈstændəd/"
+        },
+        {
+          "vn": "thể hiện cảm xúc",
+          "eng": "give an impression",
+          "type": "verb phrase",
+          "pronounce": "/ɡɪv æn ɪmˈprɛʃən/"
+        },
+      {  "vn": "sự chú ý",
+        "eng": " attention",
+        "type": "verb ",
+        "pronounce": "/əˈtɛnʃən/"
+      },
+      {  "vn": "ấn tượng",
+        "eng": " impression",
+        "type": "verb ",
+        "pronounce": "ɪmˈprɛʃən/"
+      },
+      {  "vn": "hỗ trợ",
+        "eng": " assistance",
+        "type": "verb ",
+        "pronounce": "əˈsɪstəns/"
+      },
+      {
+        "vn": "chấp nhận điều gì đó",
+        "eng": "give in",
+        "type": "phrasal verb",
+        "pronounce": "/ɡɪv ɪn/"
+      },
+      {
+        "vn": "mang lại điều gì đó",
+        "eng": "give rise to",
+        "type": "phrasal verb",
+        "pronounce": "/ɡɪv raɪz tuː/"
+      },
   ];
   
